56845efba637dfeff5857c9ce9c5b0e1c49e297f
hugh <hugh182@qq.com>
2018-03-18 18:51:58 +0800

    push_log

--- flasky/ch08/app/auth/views.py
+++ flasky/ch08/app/auth/views.py
@@ -68,6 +68,7 @@ def confirm(token):
 @auth.before_app_request
 def before_request():
     print(__file__, request.endpoint)
+    print(__file__, request.endpoint[:5])
     if current_user.is_authenticated \
         and not current_user.confirmed \
         and request.endpoint[:5] != 'auth' \
@@ -79,3 +80,12 @@ def unconfirmed():
     if current_user.is_anonymous or current_user.confirmed:
         return redirect(url_for('main.index'))
     return render_template('auth/unconfirmed.html')
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
